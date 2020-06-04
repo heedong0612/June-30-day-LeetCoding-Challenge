fba4cfb2316432779b4479080d2699826d12e30f
Jeremy Tandjung <jeremytandjung@icloud.com>
2020-06-04 11:18:31 -0700

    added day4 sol -j

---
+++ Day4/reverseString_Jeremy.py
@@ -0,0 +1,11 @@
+from typing import List
+
+def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        for i in range(len(s)//2):
+            temp = s[i]
+            back = -(i + 1)
+            s[i] = s[back]
+            s[back] = temp
