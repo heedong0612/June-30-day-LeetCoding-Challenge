1ba567ea84cd7ec4e3e1812456c403407ac473a4
Jeremy Tandjung <jeremytandjung@icloud.com>
2020-06-04 11:21:22 -0700

    added alt solution -jeremy

--- Day4/reverseString_Jeremy.py
+++ Day4/reverseString_Jeremy.py
@@ -4,8 +4,10 @@ def reverseString(self, s: List[str]) -> None:
         """
         Do not return anything, modify s in-place instead.
         """
-        for i in range(len(s)//2):
-            temp = s[i]
-            back = -(i + 1)
-            s[i] = s[back]
-            s[back] = temp
+        # for i in range(len(s)//2):
+        #     temp = s[i]
+        #     back = -(i + 1)
+        #     s[i] = s[back]
+        #     s[back] = temp
+        
+        s.reverse()
