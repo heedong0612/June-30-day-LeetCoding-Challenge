74712018944007d7a22b20c4f4ad23383a78a44e
Jeremy Tandjung <jeremytandjung@icloud.com>
2020-06-18 20:58:11 -0700

    added day18 sol -jer (from the internet) git push

--- Day18/hIndex_Jeremy.py
+++ Day18/hIndex_Jeremy.py
@@ -1,1 +1,20 @@
+from typing import List
+
 def hIndex(citations: List[int]) -> int:
+    '''
+    Got it from 
+    https://www.youtube.com/watch?v=CjKJDloMnwE
+    '''
+    size = len(citations)
+    l, r = 0, size - 1
+    
+    while l <= r:
+        mid = l + (r - l) // 2
+        if citations[mid] == size - mid:
+            return citations[mid]
+        elif citations[mid] > size - mid:
+            r = mid - 1
+        else:
+            l = mid + 1
+        
+    return n - l
